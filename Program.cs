055e8cc4d6be19be0397b03288c7843cd8fafff7
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 11:02:40 +0200

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 12. Умножение всех элементов массива на определенное число.
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int sum = 1;
+            foreach (var el in nums)
+            {
+                sum *= el;
+            }
+            Console.WriteLine($"Сумма {sum}");
+            Console.ReadLine();
         }
     }
 }

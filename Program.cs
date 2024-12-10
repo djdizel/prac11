f4329d8216f304bbac7c86b21903242cf385da7c
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:58:34 +0200

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 11. Сумма всех элементов массива.
+            int[] nums = {1,2,3,4,5,6,7,8};
+            int sum = 0;
+            foreach(var el in nums)
+            {
+                sum += el;
+            }
+            Console.WriteLine($"Сумма {sum}");
+            Console.ReadLine();
         }
     }
 }

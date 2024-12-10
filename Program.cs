931284a73f0a1a001049efc00bf2674d28206614
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:40:19 +0200

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 4.Сортировка массива по возрастанию.
+            //int[] omas = { -1, 2, -3, 4, -5 };
+            //Array.Sort(omas);
+            //foreach (int i in omas)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
         }
     }
 }

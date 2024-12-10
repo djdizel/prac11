dbb4f5ef7eddda2d29493d8e1d680616850d42ce
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:38:33 +0200

    123123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 2.Обнулить заданный диапазон элементов в одномерном массиве.
+            //int[] omas = { 1, 2, 3, 4, 5 };
+            //Console.WriteLine("массив");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //Array.Clear(omas, 2, 2);
+            //Console.WriteLine("обнуленный массив");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //Console.ReadLine();
         }
     }
 }

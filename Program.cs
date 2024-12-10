6df775362b13c083735b4ca79a4db3d56585a449
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:41:34 +0200

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 5.Реверс массива.
+            //int[] omas = { -1, 2, -3, 4, -5 };
+            //Array.Reverse(omas);
+            //foreach (int i in omas)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
         }
     }
 }

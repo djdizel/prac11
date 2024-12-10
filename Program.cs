d1cb6c2e5d63510a42ca8dff4ced0f5205f9e9fa
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:48:28 +0200

    12

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 9.Поиск индекса элемента.
+            //int[] array = { 10, 20, 30, 40, 50, 60 };
+            //Console.Write("Введите число для поиска индекса: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int index = Array.FindIndex(array, x => x == n);
+            //if (index >= 0)
+            //    Console.WriteLine($"Индекс элемента {n}: {index}");
+            //else
+            //    Console.WriteLine("Элемент не найден.");
+            //Console.ReadLine();
         }
     }
 }

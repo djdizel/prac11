f6ccc01694cbfea8aa8bc42e89edcdd77deb78df
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:52:26 +0200

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 10.Удаление всех четных элементов из массива.
+            int[] nums = {1,2,3,4,5,6,7,8};
+            Console.WriteLine("Нечетные элементы:");
+            foreach(var el in nums)
+            {
+                if(el % 2 != 0)
+                {
+                    Console.WriteLine(el);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }

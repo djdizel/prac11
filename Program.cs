9fb1027ec9e0076a67ed79a35e6650ce9f5851ec
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:46:06 +0200

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 7.Поиск элемента по условию.
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Console.WriteLine("Четные числа в массиве:");
+            //var evenNumbers = numbers.Where(n => n % 2 == 0).ToArray();
+            //if (evenNumbers.Length > 0)
+            //{
+            //    foreach (var number in evenNumbers)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Четных чисел не найдено.");
+            //}
+            //Console.ReadLine();
         }
     }
 }

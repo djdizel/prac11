d7e0909cec676eb999a7bb9a0c01524eb8397e85
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:52:53 +0200

    12

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace prac11
         static void Main(string[] args)
         {
             //Задача 1.Определить количество элементов одномерного массива.
-            //int[] omas = { 1, 2, 3, 4, 5 };
-            //Console.WriteLine($"размер: {omas.Length}");
-            //Console.ReadLine();            
+            int[] omas = { 1, 2, 3, 4, 5 };
+            Console.WriteLine($"размер: {omas.Length}");
+            Console.ReadLine();
         }
     }
 }

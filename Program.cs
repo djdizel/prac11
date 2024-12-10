c1879df5cc3d07a583aa4c4b0e72390ba4ea020b
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 11:06:24 +0200

    12

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[15];
+            Random rng = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rng.Next(-50, 51);
+            }
+            int count = 0;
+            foreach (int num in nums)
+            {
+                if (num % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Массив:");
+            foreach (int num in nums)
+            {
+                Console.WriteLine($"{num} ");
+            }
+            Console.WriteLine($"Количество элементов, кратных 3: {count}");
+            Console.ReadLine();
         }
     }
 }

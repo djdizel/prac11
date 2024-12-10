b6f954b1dad1f2a73047e2cdbc928a7dc5e0e26a
Kab-35-16 <Kab-35-16@KAB-32-8->
2024-12-10 10:42:12 +0200

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace prac11
     {
         static void Main(string[] args)
         {
+            //Задача 6. Подсчет количества вхождений определенного числа в массив.
+            //int[] omas = { 1, 2, 3, 4, 5, 2, 2, 6, 7, 2 };
+            //Console.Write("Введите число для подсчета: ");
+            //int i = int.Parse(Console.ReadLine());
+            //int count = Array.FindAll(omas, x => x == i).Length;
+            //Console.WriteLine($"Число {i} встречается {count} раза в массиве.");
+            //Console.ReadLine();
         }
     }
 }
